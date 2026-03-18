357b0d22e4b3d5d13dea79a10e662e372b4da2e3
romz987pc <eesistance@gmail.com>
2026-03-18 22:43:51 +0300

    feat: add eslint + config and lint;

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import sayHello from "../src/cli.js"
+import sayHello from '../src/cli.js'
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!')
 
-sayHello();
+sayHello()
