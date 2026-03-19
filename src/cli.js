a0c3cf03773dc0412b8a31cb3aa9221ecc8cfe69
romz987pc <eesistance@gmail.com>
2026-03-19 22:01:44 +0300

    fix: sayHello -> askQuestion;

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync'
 
-export default () => {
-  const name = readlineSync.question('May I have your name? ')
-  console.log(`Hello, ${name}`)
+export const askQuestion = (question) => {
+  return readlineSync.question(`${question} `)
 }
