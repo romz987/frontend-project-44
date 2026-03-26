ff1a38bd63337db0682464355412fec4f99c0891
romz987pc <eesistance@gmail.com>
2026-03-26 17:15:26 +0300

    fix: even game logic;

---
+++ src/games/even.js
@@ -0,0 +1,14 @@
+import { genRandomNumber } from "../utils.js";
+import runGameLogic from "../gameLogic.js";
+
+const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+const checkParity = num => num % 2 === 0
+
+const generateRound = () => {
+  const num = genRandomNumber(100)
+  const correctAnswer = checkParity(num) ? 'yes' : 'no'
+  return [`Question: ${num}`, correctAnswer]
+}
+
+runGameLogic(DESCRIPTION, generateRound)
