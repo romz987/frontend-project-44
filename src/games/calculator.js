4b3b947d5061fc17cc3edd92e3224843927d5e1c
romz987pc <eesistance@gmail.com>
2026-03-26 17:16:50 +0300

    feat: create calculator game;

---
+++ src/games/calculator.js
@@ -0,0 +1,13 @@
+import { genRandomNumber } from "../utils.js";
+import runGameLogic from "../gameLogic.js";
+
+const DESCRIPTION = "What is the result of the expression?"
+
+const generateRound = () => {
+  const firstNum = genRandomNumber(100)
+  const secondNum = genRandomNumber(100)
+  const correctAnswer = firstNum + secondNum
+  return [`${firstNum} + ${secondNum}`, correctAnswer]
+}
+
+runGameLogic(DESCRIPTION, generateRound)
