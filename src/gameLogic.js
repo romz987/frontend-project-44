b3fc19fee8918139a36f18a15e3f4832d8d51d89
romz987pc <eesistance@gmail.com>
2026-03-26 17:16:28 +0300

    feat: create game logic algorithm;

---
+++ src/gameLogic.js
@@ -0,0 +1,39 @@
+import askQuestion from "./cli.js"
+
+const runGameLogic = (description, generateRound, roundsCount = 3) => {
+  // Greeting
+  console.log('Welcome to the Brain Games!')
+  const userName = askQuestion('May I have your name?')
+  console.log(`Hello, ${userName}`)
+  console.log(description)
+
+  // Logic
+  while (roundsCount > 0) {
+    // Generate question 
+    const roundData = generateRound()
+    const question = roundData[0]
+    const correctAnswer = roundData[1].toString()
+
+    // Ask question
+    const userAnswer = askQuestion(`Question: ${question}\nYour answer:`)
+
+    // Check result
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!')
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`)
+      console.log(`Let's try again, ${userName}!`)
+      break
+    }
+
+    // Rounds counter
+    roundsCount -= 1
+  }
+
+  // Check result
+  if (roundsCount === 0) {
+    console.log(`Congratulations, ${userName}!`)
+  }
+} 
+
+export default runGameLogic
