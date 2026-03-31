875152cf94c6405a031322c11e97c78dabc939f3
romz987pc <eesistance@gmail.com>
2026-03-31 15:07:26 +0300

    feat: add generate random number in range;

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export const genRandomNumber = (max) => {
   return Math.floor(Math.random() * max)
 }
+
+export const genRandomNumberInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
