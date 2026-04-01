0b0b03d074e8e5c52d657b3acf0bbb074e88dac5
romz987pc <eesistance@gmail.com>
2026-04-01 13:10:29 +0300

    fix: style;

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,17 @@
-import { genRandomNumber } from '../utils.js'
-import runGameLogic from '../gameLogic.js'
+import { genRandomNumber } from "../utils.js";
+import runGameLogic from "../gameLogic.js";
 
-const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+const DESCRIPTION =
+  'Answer "yes" if the number is even, otherwise answer "no".';
 
-const checkParity = num => num % 2 === 0
+const checkParity = (num) => num % 2 === 0;
 
 const generateRound = () => {
-  const num = genRandomNumber(100)
-  const correctAnswer = checkParity(num) ? 'yes' : 'no'
-  return [`${num}`, correctAnswer]
-}
+  const num = genRandomNumber(100);
+  const correctAnswer = checkParity(num) ? "yes" : "no";
+  return [`${num}`, correctAnswer];
+};
 
 export default () => {
-  runGameLogic(DESCRIPTION, generateRound)
-}
+  runGameLogic(DESCRIPTION, generateRound);
+};
