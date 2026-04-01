774bfb0469b182a0ef7b88200c296c84cd7e7db2
romz987pc <eesistance@gmail.com>
2026-04-01 12:57:20 +0300

    feat: add brain-prime game;

---
+++ bin/brain-prime.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import startGame from '../src/games/prime.js'
+startGame()
---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import { genRandomNumberInRange } from '../utils.js'
+import runGameLogic from '../gameLogic.js'
+
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no"'
+
+const checkPrime = (value) => {
+  if (value < 2) return false
+  if (value === 2) return true
+  if (value % 2 === 0) return false
+  for (let i = 3; i * i <= value; i += 2) {
+    if (value % i === 0) return false
+  }
+  return true
+}
+
+const generateRound = () => {
+  const num = genRandomNumberInRange(2, 100)
+  const correctAnswer = checkPrime(num) ? 'yes' : 'no'
+  return [`${num}`, correctAnswer]
+}
+
+export default () => {
+  runGameLogic(DESCRIPTION, generateRound)
+}
