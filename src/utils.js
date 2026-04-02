a9f38c115fb01df5ae52a13a3c0aaa4c80cd9ed5
romz987pc <eesistance@gmail.com>
2026-04-02 14:46:11 +0300

    fix: exclude sonarqube check for math.random;

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const genRandomNumber = (max) => {
-  return Math.floor(Math.random() * max)
+  return Math.floor(Math.random() * max) // NOSONAR
 }
 
 export const genRandomNumberInRange = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min // NOSONAR
 }
